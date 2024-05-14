eea5e4d9da400870225a4c0927c4e7c38c27ebaf
gabrielFPG <gfg.pantoja@yavirac.edu.ec>
2024-05-14 09:50:22 -0500

    clase2

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,9 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterAutoDto } from './dto/register-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+
+    constructor(private authService: AuthService){
+        
+    }
     @Post('register')
     registerUser(@Body()userObj : RegisterAutoDto){
         console.log(userObj.email);
@@ -12,6 +17,6 @@ export class AuthController {
 
     @Post('login')
     login(@Body() credenciales: LoginAuthDto){
-        
+
     }
 }
